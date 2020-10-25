c1491235a2f1d061b09d15c35359dd198349b2da
sugarydev <sugarydev.work@gmail.com>
2020-10-26 02:42:24 +0300

    add template, static path, make site4.html

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask, render_template, request
+import os
 
-app = Flask(__name__)
+TEMPLATE_DIR = os.path.abspath("C:/Users/posta/PycharmProjects/flaskProject/templates")
+STATIC_DIR = os.path.abspath("C:/Users/posta/PycharmProjects/flaskProject/templates/static")
+
+app = Flask(__name__, template_folder=TEMPLATE_DIR, static_folder=STATIC_DIR)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('site4.html', name="Незнакомец")
+    return render_template('site4.html')
 
 
 if __name__ == '__main__':
@@ -17,4 +22,4 @@ if __name__ == '__main__':
 def welcome():
     if request.method == 'POST':
         name = request.form.get('name')
-    return render_template('site4.html', name=name)
+    return render_template('site4.html', name=name, )
